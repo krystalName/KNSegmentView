47cc968351e44190f86feaa77e90203043399971
Leessang <992275294@qq.com>
2017-11-03 10:47:05 +0800

    Initial Commit

---
+++ KNSegmentView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNSegmentView
+//
+//  Created by 刘凡 on 2017/11/3.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNSegmentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNSegmentView
+//
+//  Created by 刘凡 on 2017/11/3.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
